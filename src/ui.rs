ce830de248f80d83c1bb9268d872f621c82f7d31
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-03-20 09:52:44 -0500

    Refactor UiState

--- src/ui.rs
+++ src/ui.rs
@@ -14,20 +14,42 @@ use gtk::glib::signal::Propagation;
 #[derive(Clone)]
 pub struct UiState {
     canvas_p: Rc<RefCell<Canvas>>,
+    window: ApplicationWindow,
 }
 
 impl UiState {
     pub fn run_main_ui() -> gtk::glib::ExitCode {
+        let state = Self::new();
         let app = Application::builder().build();
 
-        app.connect_activate(move |app| Self::build_ui(app));
+        app.connect_activate(clone!(@strong state => move |app| {
+            state.borrow().window.set_application(Some(app));
+            state.borrow().window.present();
+        }));
+
         app.run()
     }
 
-    fn new() -> UiState {
-        UiState {
+    fn new() -> Rc<RefCell<UiState>> {
+        let state = Rc::new(RefCell::new(UiState {
             canvas_p: Canvas::new(mk_test_image()),
-        }
+            window: ApplicationWindow::builder()
+                .title("RS-Paint")
+                .build(),
+        }));
+
+        state.borrow().window.set_child(Some(state.borrow().canvas_p.borrow().widget()));
+
+        let key_controller = EventControllerKey::new();
+
+        key_controller.connect_key_pressed(clone!(@strong state => move |_, key, _, modifier| {
+            state.borrow_mut().handle_keypress(key, modifier);
+            Propagation::Proceed
+        }));
+
+        state.borrow().window.add_controller(key_controller);
+
+        state
     }
 
     fn handle_keypress(&mut self, key: Key, modifier: ModifierType) {
@@ -42,26 +64,5 @@ impl UiState {
                 self.canvas_p.borrow_mut().queue_redraw();
             }
         }
-    }
-
-    fn build_ui(app: &Application) {
-        let state = Rc::new(RefCell::new(Self::new()));
-
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("RS-Paint")
-            .child(state.borrow().canvas_p.borrow().widget())
-            .build();
-
-        let key_controller = EventControllerKey::new();
-
-        key_controller.connect_key_pressed(clone!(@strong state => move |_, key, _, modifier| {
-            state.borrow_mut().handle_keypress(key, modifier);
-            Propagation::Proceed
-        }));
-
-        window.add_controller(key_controller);
-
-        window.present();
     }
 }
