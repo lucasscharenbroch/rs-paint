4d85163d171c5562ce86aa23be09956d9a369215
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-03-20 15:20:26 -0500

    Clamped-cursor scroll-zooming

--- src/ui/canvas.rs
+++ src/ui/canvas.rs
@@ -4,7 +4,7 @@ use super::UiState;
 use gtk::prelude::*;
 use gtk::{Frame};
 use gtk::gdk::{ModifierType};
-use gtk::{DrawingArea, EventControllerScroll, EventControllerScrollFlags};
+use gtk::{DrawingArea, EventControllerScroll, EventControllerScrollFlags, EventControllerMotion};
 use gtk::cairo::Context;
 use gtk::cairo;
 use gtk::glib::signal::Propagation;
@@ -17,6 +17,7 @@ pub struct Canvas {
     image: Image,
     zoom: f64,
     pan: (f64, f64),
+    cursor_pos: (f64, f64),
     drawing_area: DrawingArea,
     frame: Frame,
 }
@@ -32,6 +33,7 @@ impl Canvas {
             image,
             zoom: 1.0,
             pan: (0.0, 0.0),
+            cursor_pos: (0.0, 0.0),
             drawing_area,
             frame,
         }));
@@ -47,6 +49,13 @@ impl Canvas {
 
         state.borrow_mut().frame.add_controller(scroll_controller);
 
+        let motion_controller = EventControllerMotion::new();
+        motion_controller.connect_motion(clone!(@strong state => move |_, x, y| {
+            state.borrow_mut().update_cursor_pos(x, y);
+        }));
+
+        state.borrow_mut().frame.add_controller(motion_controller);
+
         state
     }
 
@@ -60,11 +69,32 @@ impl Canvas {
         const ZOOM_FACTOR: f64 = 0.2;
 
         self.zoom += inc * self.zoom * ZOOM_FACTOR;
-        if(self.zoom > MAX_ZOOM) {
+        if self.zoom > MAX_ZOOM {
             self.zoom = MAX_ZOOM;
-        } else if(self.zoom < MIN_ZOOM) {
+        } else if self.zoom < MIN_ZOOM {
             self.zoom = MIN_ZOOM;
         }
+    }
+
+    fn update_cursor_pos(&mut self, x: f64, y: f64) {
+        self.cursor_pos = (x, y);
+    }
+
+    fn inc_zoom_around_cursor(&mut self, inc: f64) {
+        // calculate center of screen position on image
+
+        let old_zoom = self.zoom;
+
+        self.inc_zoom(inc);
+
+        let canvas_height = self.drawing_area.height() as f64;
+        let canvas_width = self.drawing_area.width() as f64;
+        let (cursor_x, cursor_y) = self.cursor_pos;
+        let target_x = (canvas_width / 2.0) - cursor_x;
+        let target_y = (canvas_height / 2.0) - cursor_y;
+
+        self.pan.0 += target_x * (self.zoom - old_zoom) / (self.zoom * old_zoom);
+        self.pan.1 += target_y * (self.zoom - old_zoom) / (self.zoom * old_zoom);
     }
 
     fn inc_pan(&mut self, dx: f64, dy: f64) {
@@ -113,12 +143,12 @@ impl Canvas {
 
     fn handle_scroll(&mut self, event_controller: &EventControllerScroll, dx: f64, dy: f64) -> Propagation {
         if event_controller.current_event_state() == ModifierType::CONTROL_MASK {
-            self.inc_zoom(dy);
-            self.queue_redraw();
+            self.inc_zoom_around_cursor(dy);
         } else {
             self.inc_pan(-dx, -dy);
-            self.queue_redraw();
         }
+
+        self.queue_redraw();
 
         Propagation::Stop
     }
