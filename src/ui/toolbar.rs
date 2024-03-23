5401fe3d93e388449e324e255b8c2a5be9ec2325
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-03-23 11:08:37 -0500

    Make toolbar buttons modal

--- src/ui/toolbar.rs
+++ src/ui/toolbar.rs
@@ -1,30 +1,75 @@
 use gtk::prelude::*;
-use gtk::{Box, Orientation, Button};
+use gtk::{Box, Orientation, ToggleButton};
 use std::rc::Rc;
 use std::cell::RefCell;
+use glib_macros::clone;
+
+#[derive(Clone, PartialEq)]
+pub enum MouseMode {
+    Cursor,
+    Pencil,
+}
 
 pub struct Toolbar {
     tbox: Box,
+    mouse_mode: MouseMode,
+    mouse_mode_buttons: Vec<MouseModeButton>,
+}
+
+struct MouseModeButton {
+    mode: MouseMode,
+    button: ToggleButton,
 }
 
 impl Toolbar {
     pub fn new() -> Rc<RefCell<Toolbar>> {
         let tbox =  Box::new(Orientation::Horizontal, 10);
 
-        let cursor_button = Button::builder()
-            .label("Cursor")
-            .build();
+        let state = Rc::new(RefCell::new(Toolbar {
+            tbox,
+            mouse_mode: MouseMode::Cursor,
+            mouse_mode_buttons: vec![],
+        }));
 
-        let pencil_button = Button::builder()
-            .label("Pencil")
-            .build();
+        const button_info: &'static [(&'static str, MouseMode)] = &[
+            ("Cursor", MouseMode::Cursor),
+            ("Pencil", MouseMode::Pencil),
+        ];
 
-        tbox.append(&cursor_button);
-        tbox.append(&pencil_button);
+        state.borrow_mut().mouse_mode_buttons = button_info.iter()
+            .map(|(text, mode)| {
+                let button = ToggleButton::builder()
+                    .label(*text)
+                    .build();
 
-        Rc::new(RefCell::new(Toolbar {
-            tbox
-        }))
+                button.connect_clicked(clone!(@strong state => move |b| {
+                    if b.is_active() {
+                        state.borrow_mut().mouse_mode = mode.clone();
+                        for other_button in state.borrow().mouse_mode_buttons.iter() {
+                            if other_button.mode != *mode {
+                                other_button.button.set_active(false);
+                            }
+                        }
+                    } else {
+                        // the only way to deactivate is to activate a different modal button
+                        b.activate();
+                    }
+                }));
+
+                state.borrow_mut().tbox.append(&button);
+
+                MouseModeButton {
+                    button,
+                    mode: mode.clone(),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        state
+    }
+
+    pub fn mouse_mode(&self) -> &MouseMode {
+        &self.mouse_mode
     }
 
     pub fn widget(&self) -> &Box {
