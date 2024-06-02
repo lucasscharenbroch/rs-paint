1e7f6fed70a262e4c0ad911234601acad9564962
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-06-02 15:09:50 -0500

    Struggle to get auto scrolling for undo entries

--- src/image/undo/tree.rs
+++ src/image/undo/tree.rs
@@ -2,7 +2,8 @@ use super::{ImageStateDiff, ImageDiff};
 
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
-use gtk::{prelude::*, pango, Box as GBox, Orientation, Align, Widget, Label};
+use glib_macros::clone;
+use gtk::{pango, prelude::*, Align, Box as GBox, Label, Orientation, ScrolledWindow, Widget};
 
 struct UndoNode {
     parent: Option<Weak<UndoNode>>,
@@ -81,6 +82,7 @@ impl UndoNode {
 pub struct UndoTree {
     root: Rc<UndoNode>,
     current: Rc<UndoNode>,
+    widget: ScrolledWindow,
 }
 
 impl UndoTree {
@@ -116,16 +118,29 @@ impl UndoTree {
 
         let root_p = Rc::new(root);
 
+        let widget = ScrolledWindow::builder()
+            .child(&*root_p.container)
+            .min_content_height(400)
+            .min_content_width(250)
+            .overlay_scrolling(true)
+            // tweak expansion/ spacing?
+            // .propagate_natural_height(true)
+            // .propagate_natural_width(true)
+            // .hexpand(true)
+            // .vexpand(true)
+            .build();
+
         UndoTree {
             root: Rc::clone(&root_p),
             current: root_p,
+            widget,
         }
     }
 
     fn update_current(&mut self, new_current: Rc<UndoNode>) {
-        self.current.set_active(false);
+        self.set_node_is_active(&*self.current, false);
         self.current = new_current;
-        self.current.set_active(true);
+        self.set_node_is_active(&*self.current, true);
     }
 
     pub fn commit(&mut self, diff: ImageStateDiff) {
@@ -158,6 +173,72 @@ impl UndoTree {
     }
 
     pub fn widget(&self) -> &impl IsA<Widget> {
-        &*self.root.container
+        &self.widget
+    }
+
+    fn set_node_is_active(&self, node: &UndoNode, is_active: bool) {
+        node.set_active(is_active);
+
+        // undo menu is up           && we're setting this node to active
+        // vvvvvvvvvvvvvvvvvvvvvvvvv    vvvvvvvvv
+        if node.widget.is_realized() && is_active {
+            println!("Im realized, {}", node.widget.is_ancestor(&self.widget));
+            println!("{:?}", node.widget.compute_point(&*self.root.container, &gtk::graphene::Point::new(1.0, 1.0)));
+            println!("{:?}", node.widget.compute_bounds(&*self.root.container));
+            println!("height/width: {} {}", node.widget.height(), node.widget.width());
+            let child = node.widget.first_child().unwrap().clone();
+            println!("XX {:?}", node.widget.compute_point(&child, &gtk::graphene::Point::new(1.0, 1.0)));
+            println!("XX {:?} {}", child.width(), child.height());
+            let parent = node.widget.parent().unwrap().clone();
+            println!("XX {:?} {}", parent.width(), parent.height());
+
+
+            node.widget.connect_baseline_child_notify(|_| {
+                println!("++++++++++++++++++++++++++draw");
+            });
+
+            /*
+            node.widget.connect_realize(|_| {
+                println!("+++++++++++++++++++++++++++++++++++++ show");
+
+            });
+
+            node.widget.realize();
+            node.widget.parent().unwrap().realize();
+            */
+
+            /*
+            println!("{:?}", node.widget.compute_point(&*self.root.container, &gtk::graphene::Point::new(0.0, 0.0)));
+            let focus_pt = node.widget.compute_point(&*self.root.container, &gtk::graphene::Point::new(0.0, 0.0)).unwrap();
+
+            let v_adjustment = self.widget.vadjustment();
+            let value = v_adjustment.value();
+            let page_size = v_adjustment.page_size();
+            let y = focus_pt.y() as f64;
+            println!("{value:}, {y:}");
+
+            if y < value {
+                v_adjustment.set_value(y);
+            } else if y > value + page_size {
+                v_adjustment.set_value(y + page_size);
+            }
+            */
+
+            /*
+            node.widget.connect_realize(clone!(@strong scroll_win, @strong root_container, @strong node_widget => move|w| {
+                let focus_pt = node_widget.compute_point(&root_container, &gtk::graphene::Point::new(0.0, 0.0)).unwrap();
+
+                let v_adjustment = scroll_win.vadjustment();
+                let lower = v_adjustment.lower();
+                let upper = v_adjustment.upper();
+                let value = v_adjustment.value();
+                let page_size = v_adjustment.page_size();
+                let y = upper - (focus_pt.y() as f64);
+                println!("{:?} {:?} {} {}", v_adjustment.lower(), v_adjustment.upper(), v_adjustment.value(), focus_pt.y());
+                // v_adjustment.set_value(value - y);
+                // (value - y) is the desired change
+            }));
+            */
+        }
     }
 }
