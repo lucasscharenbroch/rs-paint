364cfae733b8c30f303e473dde438020e4027bda
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-06-07 09:49:32 -0500

    Add undo and redo into menu

--- src/ui/menu.rs
+++ src/ui/menu.rs
@@ -50,6 +50,8 @@ pub fn mk_menu(ui_state: Rc<RefCell<UiState>>) -> (Menu, Vec<SimpleAction>) {
         .item("Quit", "quit", Box::new(clone!(@strong ui_state => move || UiState::quit(ui_state.clone()))));
 
     let edit_menu = MenuBuilder::new()
+        .item("Undo", "undop", Box::new(clone!(@strong ui_state => move || UiState::undo(ui_state.clone()))))
+        .item("Redo", "redo", Box::new(clone!(@strong ui_state => move || UiState::redo(ui_state.clone()))))
         .item("History", "history", Box::new(clone!(@strong ui_state => move || ui_state.borrow().history_popup())));
 
     // image menu helpers
