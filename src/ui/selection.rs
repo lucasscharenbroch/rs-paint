16216569675fce99c880c348bd417b9576377ed7
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2024-06-22 09:38:32 -0500

    Tweak selection drawing colors/outlines/visuals

--- src/ui/selection.rs
+++ src/ui/selection.rs
@@ -10,26 +10,33 @@ pub enum Selection {
     NoSelection
 }
 
+macro_rules! set_selection_fill_color {
+    ($cr:expr) => {
+        $cr.set_source_rgba(0.3, 0.6, 1.0, 0.5);
+    };
+}
+
+macro_rules! set_selection_outline_color {
+    ($cr:expr) => {
+        $cr.set_source_rgb(0.2, 0.2, 7.0);
+    };
+}
+
 fn draw_rect_sel(zoom: f64, x: usize, y: usize, w: usize, h: usize, cr: &Context) {
-    const DASH_LENGTH: f64 = 10.0;
     const BORDER_WIDTH: f64 = 3.0;
 
     cr.set_line_width(BORDER_WIDTH / zoom);
 
-    cr.set_dash(&[DASH_LENGTH / zoom, DASH_LENGTH / zoom], 0.0);
-    cr.set_source_rgb(1.0, 1.0, 0.0);
     cr.rectangle(x as f64, y as f64, w as f64, h as f64);
-    let _ = cr.stroke();
 
-    cr.set_dash(&[DASH_LENGTH / zoom, DASH_LENGTH / zoom], DASH_LENGTH / zoom);
-    cr.set_source_rgb(0.0, 0.0, 0.0);
-    cr.rectangle(x as f64, y as f64, w as f64, h as f64);
-    let _ = cr.stroke();
+    set_selection_fill_color!(cr);
+    let _ = cr.fill_preserve();
 
-    cr.set_dash(&[], 0.0);
+    set_selection_outline_color!(cr);
+    let _ = cr.stroke();
 }
 
-fn draw_sel_mask(image_height: usize, image_width: usize, selection_mask: &mut ImageBitmask, cr: &Context) {
+fn draw_sel_mask(zoom: f64, image_height: usize, image_width: usize, selection_mask: &mut ImageBitmask, cr: &Context) {
     assert!(image_width == selection_mask.width());
     assert!(image_height == selection_mask.height());
 
@@ -38,10 +45,13 @@ fn draw_sel_mask(image_height: usize, image_width: usize, selection_mask: &mut I
     cr.append_path(path);
 
     cr.set_fill_rule(gtk::cairo::FillRule::EvenOdd);
-    cr.set_source_rgb(0.0, 0.0, 1.0);
+    set_selection_fill_color!(cr);
     let _ = cr.fill_preserve();
 
-    cr.set_source_rgb(1.0, 0.0, 0.0);
+    const BORDER_WIDTH: f64 = 3.0;
+
+    cr.set_line_width(BORDER_WIDTH / zoom);
+    set_selection_outline_color!(cr);
     let _ = cr.stroke();
 }
 
@@ -49,10 +59,12 @@ impl Canvas {
     pub fn draw_selection_outline(&mut self, cr: &Context) {
         let image_height = self.image_height() as usize;
         let image_width = self.image_width() as usize;
+        let zoom = *self.zoom();
 
         match self.selection {
-            Selection::Rectangle(x, y, w, h) => draw_rect_sel(*self.zoom(), x, y, w, h, cr),
+            Selection::Rectangle(x, y, w, h) => draw_rect_sel(zoom, x, y, w, h, cr),
             Selection::Bitmask(ref mut selection_mask) => draw_sel_mask(
+                zoom,
                 image_height,
                 image_width,
                 selection_mask,
